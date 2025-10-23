d311ce0d9aeb3456b2495dbd9734bfc0f315954c
blakena <blakena@MD-T103-611667>
2025-10-23 09:11:00 +0800

    Add project files.

---
+++ ArrayGridDemo/ArrayBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayGridDemo
+{
+    public static class ArrayBuilder
+    {
+        // Builds a sample 2D array of doubles
+        public static double[,] BuildSampleArray(int rows, int cols)
+        {
+            var array = new double[rows, cols];
+            for (int r = 0; r < rows; r++)
+                for (int c = 0; c < cols; c++)
+                    array[r, c] = r * 100 + c;
+            return array;
+        }
+    }
+
+}
---
+++ ArrayGridDemo/DataTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayGridDemo
+{
+    public static class DataTableHelper
+    {
+        // Converts a 2D array into a DataTable for binding to a DataGrid
+        public static DataTable ToDataTable(double[,] array, string colPrefix = "C")
+        {
+            int rows = array.GetLength(0);
+            int cols = array.GetLength(1);
+
+            var table = new DataTable();
+
+            // Create columns
+            for (int c = 0; c < cols; c++)
+                table.Columns.Add($"{colPrefix}{c}", typeof(double));
+
+            // Populate rows
+            for (int r = 0; r < rows; r++)
+            {
+                var row = table.NewRow();
+                for (int c = 0; c < cols; c++)
+                    row[c] = array[r, c];
+                table.Rows.Add(row);
+            }
+
+            return table;
+        }
+    }
+}
