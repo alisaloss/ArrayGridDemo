a6bea145a81b26053486c9a5c8391c1f7defa2b7
Alisa Blakeney <blakena@cite.wa.edu.au>
2025-11-07 09:35:08 +0800

    add conditional highlighting to cells

--- ArrayGridDemo/MainWindow.xaml.cs
+++ ArrayGridDemo/MainWindow.xaml.cs
@@ -16,14 +16,44 @@ namespace ArrayGridDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const double Threshold = 300.0; // You'll be taking the threshold as user input not hardcoding
+
         public MainWindow()
         {
             InitializeComponent();
 
             var data = ArrayBuilder.BuildSampleArray(6, 8);
             Grid.ItemsSource = DataTableHelper.ToDataTable(data).DefaultView;
+        }
 
+        private void Grid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            // Use dispatcher to ensure cells are generated before coloring
+            e.Row.Dispatcher.InvokeAsync(() =>
+            {
+                foreach (var cell in FindVisualChildren<DataGridCell>(e.Row))
+                {
+                    if (cell.Content is TextBlock tb &&
+                        double.TryParse(tb.Text, out var value))
+                    {
+                        cell.Background = value > Threshold ? Brushes.Red : Brushes.White;
+                    }
+                }
+            });
+        }
 
+        private static System.Collections.Generic.IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj)
+            where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(depObj, i);
+                if (child is T t)
+                    yield return t;
+
+                foreach (var childOfChild in FindVisualChildren<T>(child))
+                    yield return childOfChild;
+            }
         }
     }
 }
